9df76c037d6e5b54b09548dde53e7f566ede5b60
InzaneMembrane <44097964+InzaneMembrane@users.noreply.github.com>
2019-11-18 12:20:59 -0600

    Update Register.java

--- Login_Password/Register.java
+++ Login_Password/Register.java
@@ -1,27 +1,38 @@
-package com.example.appdevteam;
+package com.example.budget101;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.RequiresApi;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.budget101.Data.User;
+import com.example.budget101.Database.DatabaseAccess;
 
-public class Register extends AppCompatActivity implements View.OnClickListener {
+import java.security.NoSuchAlgorithmException;
+
+public class Register extends MainActivity implements View.OnClickListener {
 
     EditText et_username, et_password;
     Button submit;
+    private DatabaseAccess access;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            super.onCreate(savedInstanceState);
+        }
         setContentView(R.layout.activity_register);
 
         et_username = (EditText) findViewById(R.id.et_username);
         et_password = (EditText) findViewById(R.id.et_password);
         submit =  (Button) findViewById(R.id.submit);
+        this.access = this.getDatabase();
 
         submit.setOnClickListener(this);
     }
@@ -33,10 +44,22 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
                 String username = et_username.getText().toString();
                 String password = et_password.getText().toString();
-                User new_user = new User(username, password);
+                User new_user;
 
+                try {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                        new_user = access.newUser(username, User.hash(password), null);
+                    }
+                }
+                catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
                 startActivity(new Intent(this, MainActivity.class));
                 break;
         }
+    }
+
+    public DatabaseAccess getDatabase () {
+        return this.access;
     }
 }
