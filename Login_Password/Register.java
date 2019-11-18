9609ac92551687f25cb0ed0975dddbf0cada8e8c
InzaneMembrane <44097964+InzaneMembrane@users.noreply.github.com>
2019-11-18 17:33:14 -0600

    Update Register.java

--- Login_Password/Register.java
+++ Login_Password/Register.java
@@ -1,4 +1,4 @@
-package com.example.budget101;
+package com.budget101;
 
 import androidx.annotation.RequiresApi;
 
@@ -9,33 +9,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.budget101.Data.User;
-import com.example.budget101.Database.DatabaseAccess;
-
-import java.security.NoSuchAlgorithmException;
+import com.budget101.Data.User;
+import com.budget101.Database.DatabaseAccess;
 
 public class Register extends MainActivity implements View.OnClickListener {
 
     EditText et_username, et_password;
     Button submit;
     private DatabaseAccess access;
-    private User new_user;
-    
+    private User new_user, u;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-       
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        
+        this.access = super.getDatabase();
         setContentView(R.layout.activity_register);
 
         et_username = (EditText) findViewById(R.id.et_username);
         et_password = (EditText) findViewById(R.id.et_password);
-        submit =  (Button) findViewById(R.id.submit);
-        this.access = this.getDatabase();
-        new_user = new User(null,null,null);
-        
+        submit = (Button) findViewById(R.id.submit);
+        new_user = new User(null, null, null);
+        u = new User(null,null,null);
         submit.setOnClickListener(this);
     }
 
@@ -43,24 +40,20 @@ public class Register extends MainActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.submit:
-
                 String username = et_username.getText().toString();
                 String password = et_password.getText().toString();
-              
-                try {                  
-                        new_user = access.newUser(username, User.hash(password), null);
-                    }
+                new_user.setPassword(password);
+                u = this.access.newUser(username, new_user.getPassword(), null);
+
+                if (u == null) {
+                    Toast.makeText(this, username + " exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, username + " created", Toast.LENGTH_SHORT).show();
+                    this.finish();
                 }
-                catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                }
-        
-                this.finish();
                 break;
         }
-    }
 
-    public DatabaseAccess getDatabase () {
-        return this.access;
     }
 }
+
